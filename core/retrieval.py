aa95adce2ba77de6012d983e3bc862f2705e83f4
UK1\MinhNg <minhng@joblogic.com>
2025-03-10 16:46:09 +0700

    DLCV update

--- core/retrieval.py
+++ core/retrieval.py
@@ -8,14 +8,23 @@ from models.xbert import BertConfig, BertForMaskedLM
 
 def main(args, config):
     text_encoder_mode = args.text_encoder
-    embed_dim = config["embed_dim"]
+    embed_dim = config["embed_dim"]  # out_features: 256
 
     tokenizer = BertTokenizer.from_pretrained(text_encoder_mode)
     bert_config = BertConfig.from_json_file(config["bert_config"])
     text_encoder = BertForMaskedLM.from_pretrained(text_encoder_mode, config=bert_config)
 
+    # in_features: 768 (must be a multiple of the number of attention heads (12))
     text_width = text_encoder.config.hidden_size
-    text_proj = nn.Linear(text_width, embed_dim)
+    text_proj = nn.Linear(text_width, embed_dim)  # 768 → 256
+
+    # # forward()
+    # # extract text features
+    # text_output = text_encoder.bert(
+    #     text2.input_ids, attention_mask=text2.attention_mask, return_dict=True, mode="text"
+    # )
+    # text_embeds = text_output.last_hidden_state
+    # text_feat = F.normalize(text_proj(text_embeds[:, 0, :]), dim=-1)
 
     print("text_width: ", text_width, text_proj)
 
