f1fd61a44fca2ab8e2f15a1f3120ba1c1353e0c7
UK1\MinhNg <minhng@joblogic.com>
2025-03-19 16:48:21 +0700

    DLCV update

--- core/models/person_search_model.py
+++ core/models/person_search_model.py
@@ -67,6 +67,8 @@ class ALBEF(nn.Module):
 
         ###  Create the queue  ###
         self.queue_size = config["queue_size"]
+        self.mlm_probability = config["mlm_probability"]
+        self.mrtd_mask_probability = config["mrtd_mask_probability"]
         self.itm_head = nn.Linear(self.text_width, 2)
         self.prd_head = nn.Linear(self.text_width, 2)
         self.mrtd_head = nn.Linear(self.text_width, 2)
@@ -186,6 +188,17 @@ class ALBEF(nn.Module):
         # Positive Relation Detection
         prd_output = self.prd_head(output_pos.last_hidden_state[:, 0, :])
         loss_prd = F.cross_entropy(prd_output, replace)
+
+        # Sensitivity-aware Learning: Masked Language Modeling + Momentum-based Replaced Token Detection
+        input_ids = text1.input_ids.clone()
+        labels = input_ids.clone()
+        mrtd_input_ids = input_ids.clone()
+        # Masked Language Modeling
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        input_ids, labels = self.mask(
+            input_ids, self.text_encoder.config.vocab_size, targets=labels, probability_matrix=probability_matrix
+        )
+
         print("loss: ", loss_cl, loss_pitm, loss_prd)
         return loss_cl
 
@@ -225,6 +238,30 @@ class ALBEF(nn.Module):
             self.idx_queue[:, : batch_size - empty] = idxs[empty:].T
         ptr = (ptr + batch_size) % self.queue_size  # move pointer
         self.queue_ptr[0] = ptr
+
+    def mask(self, input_ids, vocab_size, targets=None, masked_indices=None, probability_matrix=None):
+        print('-: ', probability_matrix)
+        if masked_indices is None:
+            masked_indices = torch.bernoulli(probability_matrix).bool()
+        print('0: ', masked_indices)
+        masked_indices[input_ids == self.tokenizer.pad_token_id] = False
+        print('1: ', masked_indices)
+        masked_indices[input_ids == self.tokenizer.cls_token_id] = False
+        print('2: ', masked_indices)
+        if targets is not None:
+            targets[~masked_indices] = -100  # We only compute loss on masked tokens
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(torch.full(input_ids.shape, 0.8)).bool() & masked_indices
+        input_ids[indices_replaced] = self.tokenizer.mask_token_id
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(torch.full(input_ids.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+        random_words = torch.randint(vocab_size, input_ids.shape, dtype=torch.long).to(input_ids.device)
+        input_ids[indices_random] = random_words[indices_random]
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        if targets is not None:
+            return input_ids, targets
+        else:
+            return input_ids
 
 
 @torch.no_grad()
