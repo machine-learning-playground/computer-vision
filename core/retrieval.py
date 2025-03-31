b6fb0d1260ac2c03a999c311748e04154fab63c7
UK1\MinhNg <minhng@joblogic.com>
2025-03-31 11:12:40 +0700

    DLCV update

--- core/retrieval.py
+++ core/retrieval.py
@@ -1,7 +1,11 @@
 import argparse
+import random
+import numpy as np
 import ruamel.yaml as yaml
 import torch
+import torch.backends.cudnn as cudnn
 
+import utils
 from dataset import create_dataset, create_loader
 from models.person_search_model import ALBEF
 from models.tokenization_bert import BertTokenizer
@@ -32,10 +36,31 @@ def train(model, data_loader, optimizer, tokenizer, epoch, warmup_steps, device,
         model(image1, image2, text_input1, text_input2, alpha=alpha, idx=idx, replace=replace)
 
 
+def seed_everything(seed):
+    # Fix the seed for reproducibility
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    cudnn.deterministic = True
+    cudnn.benchmark = True
+
+
 def main(args, config):
     device = torch.device(args.device)
+
+    seed = args.seed + utils.get_rank()
+    seed_everything(seed)
+
     train_dataset = create_dataset("ps", config)
     train_loader = create_loader(train_dataset, config["batch_size_train"])
+
+    max_epoch = config["schedular"]["epochs"]
+    warmup_steps = config["schedular"]["warmup_epochs"]
+    start_epoch = 0
+    best = 0
+    best_epoch = 0
+    best_log = ""
 
     tokenizer = BertTokenizer.from_pretrained(args.text_encoder)
     model = ALBEF(config=config, text_encoder=args.text_encoder, tokenizer=tokenizer)
@@ -51,6 +76,7 @@ if __name__ == "__main__":
     parser.add_argument("--config", default="./configs/base.yaml")
     parser.add_argument("--text_encoder", default="bert-base-uncased")
     parser.add_argument("--device", default="cuda")
+    parser.add_argument("--seed", default=42, type=int)
     args = parser.parse_args()
 
     yaml_loader = yaml.YAML()
