3fc861b68ef98cb64010cfc5ba75d8d29e95f82f
MikeYuen <nhatminh.150596@gmail.com>
2025-04-27 09:57:32 +0700

    DLCV train only

--- vfe-tps/main.py
+++ vfe-tps/main.py
@@ -6,14 +6,10 @@ import random
 import time
 
 from datasets import build_dataloader
-# from processor.processor import do_train
-# from utils.checkpoint import Checkpointer
+from model import build_model
+from processor import do_train
 from utils.iotools import save_train_configs
 from utils.logger import setup_logger
-
-# from solver import build_optimizer, build_lr_scheduler
-# from model import build_model
-# from utils.metrics import Evaluator
 from utils.options import get_args
 from utils.common import get_rank, synchronize
 
@@ -51,29 +47,15 @@ if __name__ == "__main__":
 
     # get image-text pair datasets dataloader
     train_loader, val_img_loader, val_txt_loader, num_classes = build_dataloader(args)
-    # model = build_model(args, num_classes)
-    # logger.info("Total params: %2.fM" % (sum(p.numel() for p in model.parameters()) / 1000000.0))
-    # model.to(device)
+    model = build_model(args, num_classes)
+    logger.info("Total params: %2.fM" % (sum(p.numel() for p in model.parameters()) / 1000000.0))
+    model.to(device)
 
-    # if args.distributed:
-    #     model = torch.nn.parallel.DistributedDataParallel(
-    #         model,
-    #         device_ids=[args.local_rank],
-    #         output_device=args.local_rank,
-    #         # this should be removed if we update BatchNorm stats
-    #         broadcast_buffers=False,
-    #     )
-    # optimizer = build_optimizer(args, model)
-    # scheduler = build_lr_scheduler(args, optimizer)
+    start_epoch = 1
 
-    # is_master = get_rank() == 0
-    # checkpointer = Checkpointer(model, optimizer, scheduler, args.output_dir, is_master)
-    # # checkpointer.load(f=op.join('/home/xl/project/IRRA-main/data', 'best.pth'))
-    # evaluator = Evaluator(val_img_loader, val_txt_loader)
-
-    # start_epoch = 1
-    # if args.resume:
-    #     checkpoint = checkpointer.resume(args.resume_ckpt_file)
-    #     start_epoch = checkpoint["epoch"]
-
-    # do_train(start_epoch, args, model, train_loader, evaluator, optimizer, scheduler, checkpointer)
+    do_train(
+        start_epoch,
+        args,
+        model,
+        train_loader,
+    )
